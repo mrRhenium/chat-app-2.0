d49352576000ecbb0bff79a265de6ebc50e5e35d
mr.Rhenium <mr.rhenium75614@gmail.com>
2023-05-23 12:32:52 +0530

    add picture page which is not setUP

---
+++ src/app/profile/[username]/picture/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const Picture = ({ item }) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <section className={style.header}>
+        <div className={style.left_cover}>
+          <span
+            className={style.back_btn}
+            onClick={() => {
+              router.back();
+            }}
+          >
+            <BsArrowLeft className={style.icons} />
+          </span>
+          <span className={style.name_info}>
+            <p className={style.name}>{item.username}</p>
+          </span>
+        </div>
+        <div className={style.right_cover}>
+          <span className={style.opt}>
+            <CiMenuKebab className={style.icons} />
+          </span>
+        </div>
+      </section>
+      {/* Profil Page Header Part Start */}
+
+      {/* Profil Page Body Part Start */}
+      <section className={style.body}></section>
+      {/* Profil Page Body Part Start */}
+    </>
+  );
+};
+
+export default Picture;
